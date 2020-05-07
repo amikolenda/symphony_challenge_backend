7af458bee4c14dbbb2920b4af0bca5c25b66cc8c
amikolenda <aminakolenda@gmail.com>
2020-05-07 12:06:47 +0200

    Add files via upload

---
+++ src/test/java/is/symphony/collegeinternship/olympicgames/OlympicgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package is.symphony.collegeinternship.olympicgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlympicgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
